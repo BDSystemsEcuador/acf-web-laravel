990b085216dc94b8b0bb64df355a67b8c7cb57e9
Byron Delgado <byrondanipm@gmail.com>
2021-05-17 12:03:00 -0500

    wsp

--- resources/views/componentes/contacto.blade.php
+++ resources/views/componentes/contacto.blade.php
@@ -1,6 +1,6 @@
 <div class="contactos">
     <img id="btn-wsp" class="contacto" src="{{asset('img/WhatsApp.png')}}" alt="">
-    <div class="contactos-wsp container" id="wsp-modal">
+    <div class="contactos-wsp" id="wsp-modal">
       <div class="contactos-container">
         <a class="wsp-num wsp-num--active" title="Click para chatear"  href="https://api.whatsapp.com/send?phone=593995952466&text=Me%20gustaría%20hablar%20con%20alguien" target="_blank" rel="noopener"><i class="fab fa-whatsapp"></i> Quito</a>
         <a class="wsp-num" title="Click para chatear"  href="https://api.whatsapp.com/send?phone==593984668316&text=Me%20gustaría%20hablar%20con%20alguien" target="_blank" rel="noopener"><i class="fab fa-whatsapp"></i> Lago Agrio</a>
